190bce0c601ec55c756dd087e7eb74c7258c422c
edsun <edsun.tabar@gmail.com>
2022-10-18 23:46:49 +0800

    fixtest #1 initial

--- #1/src/index.ts
+++ #1/src/index.ts
@@ -1,7 +1,21 @@
 import { AccountEvents } from '../../events';
 
 export function calculateAccountBalance(events: typeof AccountEvents, accountId: string): number {
-  return 0;
+  let curAmount = 0
+  for(let event of events) {
+    if (event.aggregateId === accountId) {
+      if (event.type === 'BalanceCredited') {
+        if (event.body?.amount) {
+          curAmount = curAmount + event.body?.amount
+        }        
+      } else if (event.type === 'BalanceDebited') {
+        if (event.body?.amount) {
+          curAmount = curAmount - event.body?.amount
+        }
+      }
+    }
+  }
+  return curAmount;
 }
 
 export function getAccountInformation(events: typeof AccountEvents, accountId: string) {
